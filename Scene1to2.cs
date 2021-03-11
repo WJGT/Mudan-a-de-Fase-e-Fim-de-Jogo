c65f01b6f4cef149318ae8af077617008b04fb36
WJGT <65237335+WJGT@users.noreply.github.com>
2021-03-11 17:03:38 -0300

    Add files via upload

---
+++ Scene1to2.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Scene1to2 : MonoBehaviour
+{
+    public void Scene1to2()
+    {
+        //Debug.log("HIT")
+        Application.Quit();
+        //SceneManager.LoadScene("Salataca")
+    }
+
+    public void EndOfGame()
+    {
+        Application.Quit();
+    }
+   
+    
+}
